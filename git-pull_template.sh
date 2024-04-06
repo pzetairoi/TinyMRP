aca59c7f5e8dd9152c9aff1e59e5f90190dc2993
Francisco Quesada <fcoquesada@gmail.com>
2024-04-06 18:14:49 +1100

    modified git-pull_template.sh

--- git-pull_template.sh
+++ git-pull_template.sh
@@ -1,6 +1,9 @@
+#This script is used to pull the latest code from the git repository and restart the server.
 #!/bin/bash
 cd ~/TinyMRP/SourceCode 
 git pull
+systemctl restart newtinymrp_server.service
+
 
 
 
