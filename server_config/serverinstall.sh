888a4e04490aefb5534e81a6db54e955bbe0a41b
Francisco Quesada <fcoquesada@gmail.com>
2024-04-13 17:37:08 +1000

    added severinstall.sh  to start updating the auto install script

---
+++ server_config/serverinstall.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+package_list="nginx git samba python3 net-tools"
+deliverables="3mf datasheet dxf edr pdf pic png reports step temp"
+tinyfolder="/home/tinymrp/tinymrp"
+filefolder="/home/tinymrp/Fileserver/Deliverables"
