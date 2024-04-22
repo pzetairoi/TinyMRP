7bcc0f26869941cdc55c02d85011ad3d7e8d1f85
Francisco Quesada <fcoquesada@gmail.com>
2024-04-22 14:52:02 +1000

    excel compile blank

--- install/manual processes.sh
+++ install/manual processes.sh
@@ -13,7 +13,8 @@ sudo mkdir /Fileserver/Deliverables/png
 sudo mkdir /Fileserver/System
 
 #Share with full write access
- sudo chmod +777 /Fileserver -R
+ sudo chmod +777 /Fileserver
+ sudo chmod +777  ~/Server/
 
 sudo cp  ~/Server/TinyMRP/install/smb.conf /etc/samba/smb.conf 
 
@@ -46,13 +47,14 @@ pip install -r requirements.txt
 
 #Install Mongodb
 sudo apt-get install gnupg curl -y
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt-get update
+# curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+#    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+#    --dearmor
+# echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+# sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl enable mongod.service
+sudo systemctl start mongod.service
 
 #Modify the mongodb configuration with the repository one
 sudo cp ~/Server/TinyMRP/install/mongod.conf /etc/mongod.conf
@@ -68,7 +70,7 @@ rm -rf /tmp/mongodb_backup
 
 #Copy generic image logo to Fileserver/System
 cd ~/Server/TinyMRP
-cp app/static/images/logo.png /Fileserver/System/logo.png
+sudo cp app/static/images/logo.png /Fileserver/System/logo.png
 
 
 
