5a793ce5c23ad5ccf6a0557fc004af53d15d92e6
Francisco Quesada <fcoquesada@gmail.com>
2024-05-26 15:15:49 +1000

    a

--- app/tinylib/publisher.py
+++ app/tinylib/publisher.py
@@ -238,7 +238,7 @@ def index_page(canvas,doc):
     logo="custom_files/companylogo.png"
     logoheight=0.8*inch    
    
-    canvas.drawImage(logo,-PAGE_WIDTH/2-40*mm, PAGE_HEIGHT-1.2*inch,height=logoheight, 
+    canvas.drawImage(logo,-PAGE_WIDTH/2-80*mm, PAGE_HEIGHT-1.2*inch,height=logoheight, 
                      preserveAspectRatio=True, mask='auto')
     
 
